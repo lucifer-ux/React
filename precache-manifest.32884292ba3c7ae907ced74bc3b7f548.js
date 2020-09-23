432ed978b5f59aae42639fb5fb6d95ff2f050ea5
lucifer-ux <shashwat.cool14@gmail.com>
2020-09-23 19:08:31 +0530

    Updates

---
+++ precache-manifest.32884292ba3c7ae907ced74bc3b7f548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5a06e0c242dd3d133d4b71171766b7",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "0250bbb035ed17344137",
+    "url": "/React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "651acd6ba6a39797f958",
+    "url": "/React/static/js/2.73be0ec9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.73be0ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0250bbb035ed17344137",
+    "url": "/React/static/js/main.725c9f75.chunk.js"
+  },
+  {
+    "revision": "92dfa9a597fcef830cec",
+    "url": "/React/static/js/runtime-main.b9a8e852.js"
+  }
+]);
